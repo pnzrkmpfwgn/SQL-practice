86ab3365bac5e4bb7db05f7fd70963493b435bc7
Ayberk Yigit <kurtknispel9@gmail.com>
2021-04-22 02:16:45 +0300

    Constrains

--- DB.sql
+++ DB.sql
@@ -1,12 +1,16 @@
+DROP TABLE student;
+
 CREATE TABLE student (
-    student_id INT,
-    name VARCHAR(20),
-    major VARCHAR(20),
+    student_id INT AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    major VARCHAR(20) /*UNIQUE*/DEFAULT 'undecided',
     PRIMARY KEY(student_id)
 );
 
 SELECT * FROM student;
 
-/*INSERT INTO student VALUES(1, 'Jack', 'Biology');*/
-/*INSERT INTO student VALUES(2,'Kate','Sociology');*/
-INSERT INTO student(student_id,name) VALUES(3,'Claire');/*Third parameter will be NULL by default*/
+INSERT INTO student(name) VALUES('Jack');
+INSERT INTO student(name,major) VALUES('Kate','Sociology');
+INSERT INTO student(student_id,name) VALUES(3,'Claire');
+INSERT INTO student VALUES(4,'Jack','Biology');
+INSERT INTO student VALUES(5,'Mike','Computer Science');
