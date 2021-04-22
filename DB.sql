99cc49523e385cbdf1407d0592a3f03e4265d364
Ayberk Yigit <kurtknispel9@gmail.com>
2021-04-22 22:40:25 +0300

    Union

--- DB.sql
+++ DB.sql
@@ -1,16 +1,20 @@
--- % = any # characters, _ = one character
+--Find a list of employee and branch names
+SELECT first_name /*, last_name (!) you can't use this like this because branch_name has no last_name column */
+FROM employee
+UNION
+SELECT branch_name 
+FROM branch;
+/* We can have UNION operator to unify another table */
 
---Find all the client's who are an LLC
-SELECT * FROM client
-WHERE client_name LIKE '%LLC';
+--Find the list of all clients & branch suppliers' name
+SELECT client_name, branch_id /*In here both tables have branch_id (One table have it as primary key and other have it as foreign key) so we can use this like this.*/
+FROM client
+UNION
+SELECT supplier_name, branch_id
+FROM branch_supplier;
 
---Find any branch suppliers who are in the label bussiness
-SELECT * FROM branch_suppliers 
-WHERE supplier_name LIKE '% label%';
-
---Find any employee born in October
-SELECT *
-FROM employee WHERE birth_date LIKE '____-10%';
-
---Find any clients who are schools
-SELECT * FROM client WHERE client_name LIKE '%school%';
+--Find a list of all money spent or earned by the company
+SELECT salary FROM employee 
+UNION
+SELECT total_sales FROM works_with;
+ 
